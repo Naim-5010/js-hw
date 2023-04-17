20f8f8be980e60904c53ab4b2ab7616b43614bce
Naim-5010 <mdnaim01700668591@gmail.com>
2023-04-17 15:53:55 +0600

    main file

---
+++ modul-11-hw/js.js
@@ -0,0 +1,37 @@
+
+
+        //NO 11
+        function multiplicationTable(naim) {
+                for (let i = 1; i <= 10; i++) {
+                    console.log(naim + " x " + i + " = " + naim * i);
+                }
+            }
+
+            //NO 2
+
+            function multiplicationTable(num) {
+                    for (let i = 1; i <= 10; i++) {
+                       
+                    }
+                }
+
+                //3
+                function multiplicationTable(num) {
+             for (let i = 1; i <= 10; i++) {
+                let product = num * i;
+               }
+}
+         
+
+    //NO 4
+
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        let product = num * i;
+        console.log(num + " x " + i + " = " + product);
+    }
+}
+
+
+
+
